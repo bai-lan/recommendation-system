616c8ec0b1dbf0a979581d91d302679664fa7e4d
白蘭 <bai-lan@outlook.com>
2021-11-16 20:23:13 +0800

    add anime_clean_capital.csv

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 app = flask.Flask(__name__, template_folder='templates')
 
 #read data
-df2 = pd.read_csv('./model/anime_clean.csv')
+df2 = pd.read_csv('./model/anime_clean_capital.csv')
 
 count = CountVectorizer(stop_words='english')
 count_matrix = count.fit_transform(df2['soup'])
@@ -17,6 +17,7 @@ cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
 df2 = df2.reset_index()
 indices = pd.Series(df2.index, index=df2['title'])
 all_titles = [df2['title'][i] for i in range(len(df2['title']))]
+
 
 def get_recommendations(title):
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
@@ -44,11 +45,16 @@ def main():
     if flask.request.method == 'POST':
         m_name = flask.request.form['movie_name']
         m_name = m_name.title()
+        print('*' + m_name + '*')
+        print('*Stratos 4 OVA: Stratos 4 1 Dutch Roll*')
 #        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
         if m_name not in all_titles:
+            # print("*" + m_name + '*')
+            # print(all_titles)
             return(flask.render_template('negative.html',name=m_name))
         else:
             result_final = get_recommendations(m_name)
+            print(result_final)
             names = []
             dates = []
             for i in range(len(result_final)):
